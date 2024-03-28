bc491ea911a78b0fe7aa0a1482abf1b8623765c0
jayparikh1998 <67886105+jayparikh1998@users.noreply.github.com>
2024-03-28 16:32:47 -0600

    Completed Project 2
    Added database ERD, schema, and import for the crowdfunding ETL project.

---
+++ crowdfunding_analysis.sql
@@ -0,0 +1,11 @@
+-- Import for Crowd Funding Database
+
+-- Imported data directly using "Import/Export Data" option when right-clicking the variables.
+-- Selected appropriate dataset for each of the files
+  
+-- Verify tables for correct data
+SELECT * FROM contacts limit 5;
+SELECT * FROM Category LIMIT 5;
+SELECT * FROM Subcategory LIMIT 5;
+SELECT * FROM campaign LIMIT 5;
+
